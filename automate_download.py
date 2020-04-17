503e174c28318fc928bf5523f52da55eb8c03244
WenkChr <christopher.wenkoff@canada.ca>
2020-04-17 14:40:27 -0400

    Finished initial download and filter script and created python toolbox

--- automate_download.py
+++ automate_download.py
@@ -1,13 +1,12 @@
 import os, sys, arcpy
 from arcgis.gis import GIS
-from zipfile import ZipFile
 
 arcpy.env.overwriteOutput = True
 
-def auto_download_data(data_url, file_name, outGDB, outname):
+def auto_download_data(data_url, outGDB, outname, att_or_geo, fieldPrefix= 'WC2021NGD_AL_20200313_'):
     """ Requires you to be logged into arcgis pro on this computer with an account that has access to the data you are trying to 
     download. You must be logged into arcGIS pro with your statscan AGOL account in order for this to work. This funtion will
-    go into the NGD group via the arcGIS portal and download a specific feature layer as determined by the file_name variable"""
+    go into the NGD group via the arcGIS portal and download a specific feature layer as determined by the variable"""
     #Verify credentials
     gis = GIS('pro')
     print('Logged in as: ' + str(gis.properties.user.username))
@@ -15,14 +14,33 @@ def auto_download_data(data_url, file_name, outGDB, outname):
     if os.path.split(data_url)[1] != '0':
         data_url = os.path.join(data_url, '0')
     
-    arcpy.FeatureClassToFeatureClass_conversion(data_url, outGDB, outname)
+    if att_or_geo:
+        arcpy.FeatureClassToFeatureClass_conversion(data_url, outGDB, outname, whereClause= fieldPrefix + "NGD_UID IS NOT NULL")
+    if not att_or_geo:
+        arcpy.FeatureClassToFeatureClass_conversion(data_url, outGDB, outname, whereClause= fieldPrefix + "NGD_UID IS NULL")
     return os.path.join(outGDB, outname)
-    
 
-def filter_data_NGD_Redline(Redline_data, outGDB):
-    fl = arcpy.MakeFeatureLayer_management(Redline_data, 'fl')
-    arcpy.SelectLayerByAttribute_management(fl, where_clause= "NGD_UID IS NOT NULL")
-    
+def unique_values(fc, field):
+    # Returns a list off unique values for a given field in the unput dataset
+    with arcpy.da.SearchCursor(fc, field_names= [field]) as cursor:
+        return sorted({row[0] for row in cursor})
+
+def filter_data_remove_duplicates(Redline_data, outGDB, outName, field_prefix= 'WC2021NGD_AL_20200313_'):
+    # Start by finding the most recent date for each NGD_UID and keeping only that date
+    KeepRows = []
+    for uid in unique_values(Redline_data, field_prefix + 'NGD_UID'):
+        fieldnames = [field_prefix + 'NGD_UID', 'EditDate']
+        fl = arcpy.MakeFeatureLayer_management(Redline_data, 'fl', where_clause= fieldnames[0] + '= ' + str(uid))
+        max_date = max(unique_values(fl, fieldnames[1]))
+        with arcpy.da.SearchCursor(fl, field_names= ['OBJECTID', fieldnames[1]]) as cursor:
+            for row in cursor:
+                if row[1] == max_date:
+                    KeepRows.append(row[0])
+    fl = arcpy.MakeFeatureLayer_management(Redline_data, 'fl2')
+    for oid in KeepRows:
+        arcpy.SelectLayerByAttribute_management(fl, 'ADD_TO_SELECTION', "OBJECTID = {}".format(oid))
+    arcpy.FeatureClassToFeatureClass_conversion(fl, outGDB, outName)
+    return os.path.join(outGDB, outName)
 
 # inputs
 
@@ -34,7 +52,7 @@ o_name = 'RedLine'
 #Calls
 
 results = auto_download_data(url, file_name, o_gdb, o_name)
-filtered = filter_data_NGD_Redline(results, o_gdb)
+filtered = filter_data_remove_duplicates(results, o_gdb, 'Redline_w_NGD_UID')
 
 print('DONE!')
 
