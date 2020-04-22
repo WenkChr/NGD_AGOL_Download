d188a68b8a36c581d6f27a562f124b602cca34ad
Reg Maltais <reginald.maltais@gmail.com>
2020-04-22 11:33:22 -0400

    rewrite to reflect change type process flow.

--- ngdal_change_types.py
+++ ngdal_change_types.py
@@ -1,10 +1,23 @@
 # Compare the redline NGD_AL data against the source data to detect changes
 
+# Changes that get pushed to the geometry changes workflow:
+# 1. No NGD_UID (wholly new geometry)
+# 2. The geometry has changed by more than 10m
+# 3. Right side has different name flag
+# All other records will be put into a single output to look for attribute changes
+
 import geopandas as gpd
 
-# paths to files
+# how much leeway is there in matching geometries (0.1 == 10m)
+GEOM_ROUNDING_FACTOR = 0.1
+
+# paths to input files
 redline_path = '../redline_ngdal.geojson'
 ngdal_path = '../ngdal_affected.geojson'
+
+# paths to output files
+redline_attr_changes = '../redline_attr_changes.geojson'
+redline_geom_changes = '../redline_geom_chnages.geojson'
 
 # the columns to be used for equality comparisons
 compare_cols = ['AFL_VAL', 'AFL_SFX', 'AFL_SRC', 'ATL_VAL', 'ATL_SFX', 'ATL_SRC',
@@ -17,31 +30,51 @@ output_cols = ['SGMNT_TYP_CDE', 'SGMNT_SRC', 'STR_CLS_CDE', 'STR_RNK_CDE',
 # load the data to be compared
 print("Loading datasets from disk.")
 redline = (gpd.read_file(redline_path)
-        .to_crs("EPSG:3347")
-        .drop_duplicates(subset="NGD_UID", keep="last"))
-# remove any null values (new geometries)
-redline = redline[redline["NGD_UID"].notna()].set_index("NGD_UID", drop=False)
-# convert the NGD_UID values to integer
-redline["NGD_UID"] = redline["NGD_UID"].astype(int)
+        .to_crs("EPSG:3347"))
+ngdal = gpd.read_file(ngdal_path)
 
-ngdal = (gpd.read_file(ngdal_path)
-        .set_index("NGD_UID"))
+# Step 1 - break away any records without an NGD_UID
+print("Looking for new geometries.")
+geom_change = redline.loc[redline["NGD_UID"].isna()]
 
-# get the length as a proxy for geometry comparisons
-redline['length'] = redline.geometry.length.round(0)
-ngdal['length'] = ngdal.geometry.length.round(0)
+attr_change = redline.loc[redline["NGD_UID"].notna()]
+attr_change["NGD_UID"] = attr_change["NGD_UID"].astype(int)
+attr_change = attr_change.drop_duplicates(subset="NGD_UID", keep="last")
 
-print("Determining attribute changes.")
-change_mask = redline[compare_cols].ne(ngdal[compare_cols])
-changed = redline.where(change_mask)
-# only mask columns will have a value, so drop anything else that was there and merge it back.
-changed = changed[compare_cols]
-changed = changed.merge(redline[output_cols], left_index=True, right_index=True)
+print("Geometry changes: ", len(geom_change))
+print("Attibute changes: ", len(attr_change))
 
-# split the changed data based on if there is a geometry change
-changed_geom = redline[changed['length'].notna()]
-changed_attr = changed[changed['length'].isna()]
+# Step 2 - look for any geometries that have a >10m change
+print("Looking for changed geometries")
+attr_change['geom_threshold'] = (attr_change.geometry.length * GEOM_ROUNDING_FACTOR).round(0)
+ngdal['length_threshold'] = (ngdal.geometry.length * GEOM_ROUNDING_FACTOR).round(0)
 
-print("Writing changed data to file.")
-changed_attr.to_csv("../redline_ngdal_attr_changed.csv")
-changed_geom.to_file("../redline_ngdal_geom_changed.geojson", driver='GeoJSON', index=False)
+geom_change_detect = attr_change.merge(ngdal[['NGD_UID','length_threshold']], on='NGD_UID')
+is_geom_change = geom_change_detect[geom_change_detect['geom_threshold'] != geom_change_detect['length_threshold']]
+geom_change_uids = is_geom_change['NGD_UID'].unique().tolist()
+
+# add the geometry changes to the set with no NGD_UID
+geom_change = geom_change.append(attr_change[attr_change['NGD_UID'].isin(geom_change_uids)])
+
+# remove any newly identified geometry changes from the attr_change dataframe
+attr_change = attr_change[~attr_change['NGD_UID'].isin(geom_change_uids)]
+
+print("Geometry changes: ", len(geom_change))
+print("Attibute changes: ", len(attr_change))
+
+# Step 3 - look for any records identified as having a different street name on either side of the arc
+print("Looking for right side street name differences")
+diff_rh = attr_change[attr_change['STR_RH_DIFF_FLG'] == 1]
+geom_change_uids = diff_rh['NGD_UID'].unique().tolist()
+# add the geometry changes to the set with no NGD_UID
+geom_change = geom_change.append(attr_change[attr_change['NGD_UID'].isin(geom_change_uids)])
+# remove any newly identified geometry changes from the attr_change dataframe
+attr_change = attr_change[~attr_change['NGD_UID'].isin(geom_change_uids)]
+
+print("Geometry changes: ", len(geom_change))
+print("Attibute changes: ", len(attr_change))
+
+# Write the data to disk
+print("Writing change results to disk.")
+geom_change.to_file("../redline_geom_changes.geojson", driver='GeoJSON')
+attr_change.to_file("../redline_attr_changes.geojson", driver='GeoJSON')
