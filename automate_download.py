b816b5b8273e49194a9d92a440637f969413933b
WenkChr <christopher.wenkoff@canada.ca>
2020-04-23 14:15:22 -0400

    removed useless code and cleaned up comments

--- automate_download.py
+++ automate_download.py
@@ -6,7 +6,8 @@ arcpy.env.overwriteOutput = True
 def auto_download_data(data_url, outGDB, outname, att_or_geo, from_date, to_date, fieldPrefix= 'WC2021NGD_AL_20200313_'):
     """ Requires you to be logged into arcgis pro on this computer with an account that has access to the data you are trying to 
     download. You must be logged into arcGIS pro with your statscan AGOL account in order for this to work. This funtion will
-    go into the NGD group via the arcGIS portal and download a specific feature layer as determined by the variable"""
+    go into the NGD group via the arcGIS portal and download a specific feature layer as determined by the variable. Date format
+    for the from and too dates are YYYY-MM-DD. Do not include a timestamp as that will cause errors"""
     #Verify credentials
     gis = GIS('pro')
     print('Logged in as: ' + str(gis.properties.user.username))
@@ -81,14 +82,14 @@ def unique_values(fc, field):
     with arcpy.da.SearchCursor(fc, field_names= [field]) as cursor:
         return sorted({row[0] for row in cursor})
 
-def filter_data_remove_duplicates(Redline_data, outGDB, outName, field_prefix= 'WC2021NGD_AL_20200313_'):
+def filter_data_remove_duplicates(Redline_data, outGDB, outName):
     # Start by finding the most recent date for each NGD_UID and keeping only that date
     KeepRows = []
-    for uid in unique_values(Redline_data, field_prefix + 'NGD_UID'):
+    for uid in unique_values(Redline_data, 'NGD_UID'):
         if uid == None:
             print('NGD_UID is None no filtering required')
             break
-        fieldnames = [field_prefix + 'NGD_UID', 'EditDate']
+        fieldnames = ['NGD_UID', 'EditDate']
         fl = arcpy.MakeFeatureLayer_management(Redline_data, 'fl', where_clause= fieldnames[0] + '= ' + str(uid))
         max_date = max(unique_values(fl, fieldnames[1]))
         with arcpy.da.SearchCursor(fl, field_names= ['OBJECTID', fieldnames[1]]) as cursor:
@@ -100,16 +101,15 @@ def filter_data_remove_duplicates(Redline_data, outGDB, outName, field_prefix= '
     arcpy.FeatureClassToFeatureClass_conversion(fl, outGDB, outName)
     return os.path.join(outGDB, outName)
 
-def address_field_check(redline_data, out_gdb, out_base_name, field_prefix= 'WC2021NGD_AL_20200313_'):
+def address_field_check(redline_data, out_gdb, out_base_name):
     print('running address field changes check')
     # check redline fields against the NGD_AL fields. If fields change from valid to invalid flag those rows
     fields_to_qc = ['NGD_UID','AFL_VAL','AFL_SRC', 'AFR_VAL', 'AFR_SRC', 'ATL_VAL', 'ATL_SRC', 'ATR_VAL', 'ATR_SRC', 'ADDR_TYP_',
                     'ADDR_TYP1', 'ADDR_PRTY', 'ADDR_PR_1'] # ADDR_PRTY is ADDR_PRTY_L and ADDR_PR_1 is ADDR_PRTY_R TYP_ is L and TYP1 is R
-    fields_w_pre = [field_prefix + f for f in fields_to_qc]
     # filter NGD_AL so that it only contains those NGD_UIDs contained in the Redline
     fail_rows = []
     good_rows = []
-    with arcpy.da.SearchCursor(redline_data, field_names= fields_w_pre ) as cursor:
+    with arcpy.da.SearchCursor(redline_data, field_names= fields_to_qc ) as cursor:
         for row in cursor:
             # if AFL_VAL IS NOT NULL THEN ATL_VAL, ADDR_TYP_L, ADDR_PRTY_L NOT NULL
             if row[1] != None and row[5] == None or  row[9] == None or row[11] == None:
@@ -123,9 +123,9 @@ def address_field_check(redline_data, out_gdb, out_base_name, field_prefix= 'WC2
             good_rows.append(row[0])
     print('exporting sorted rows')
     arcpy.FeatureClassToFeatureClass_conversion(redline_data, out_gdb, out_base_name + '_goodAddr',  
-                                                where_clause= field_prefix + 'NGD_UID IN' + str(tuple(good_rows)))
+                                                where_clause= 'NGD_UID IN' + str(tuple(good_rows)))
     arcpy.FeatureClassToFeatureClass_conversion(redline_data, out_gdb, out_base_name + '_badAddr',  
-                                                where_clause= field_prefix + 'NGD_UID IN' + str(tuple(fail_rows)))
+                                                where_clause= 'NGD_UID IN' + str(tuple(fail_rows)))
 
 
 
@@ -141,21 +141,8 @@ o_name = 'RedLine_datetest'
 print('Running calls')
 results = auto_download_data(url, o_gdb, o_name, True, '2020-04-10', '2020-04-15')
 rename_the_fields(results)
-#results = os.path.join(o_gdb,o_name)
-sys.exit()
 print('Filtering')
 filtered = filter_data_remove_duplicates(results, o_gdb, 'Redline_w_NGD_UID')
 print('Running address field QC checks')
 #address_field_check(filtered, o_gdb, o_name)
 print('DONE!')
-
-# NGD_AL = download_agol_ngd_files(r'https://services7.arcgis.com/bRi0AN5rG57dCDE4/arcgis/rest/services/WC2021NGD_AL_20200313_WITH_STRTBL/FeatureServer/0', 
-  #                              o_gdb, 'NGD_AL', unique_values(filtered, 'WC2021NGD_AL_20200313_NGD_UID'))
-    # group = gis.groups.search('title: ' + group) [0]
-    # items = group.content()
-    # item_to_download = [d_item for d_item in items if d_item.title == file_name]
-    # if len(item_to_download) == 0:
-    #     print('No items of that name exist. Try again')
-    #     return None
-    # item_url = item_to_download[0].url
-    # o_file = gis.content.get(item_url)
