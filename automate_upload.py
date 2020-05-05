4425996ae6421ea796c8c6de58ba5bb09e1c9825
WenkChr <christopher.wenkoff@canada.ca>
2020-05-05 11:19:48 -0400

    Streamlined workflow, shares to NGD and deletes all same name files in AGOL. bases name off current date

--- automate_upload.py
+++ automate_upload.py
@@ -1,40 +1,35 @@
 import arcpy, os, sys
+import pandas as pd
+from dotenv import load_dotenv
 from arcgis import GIS
-from zipfile import ZipFile
+from arcgis.features import GeoAccessor
+from datetime import date
 
 arcpy.env.overwriteOutput = True
 
 #---------------------------------------------------------------------------
-#Functions
-
-def upload_data(in_geo_data, outDir, properties_dict):
-    # SRC: https://developers.arcgis.com/labs/python/import-data/ 
-    simple_name =  os.path.split(in_geo_data)[1]
-    inFile = in_geo_data
-    if arcpy.Describe(inFile).dataType != 'ShapeFile':
-        arcpy.FeatureClassToFeatureClass_conversion(in_geo_data, outDir, simple_name)
-        inFile = os.path.join(outDir, simple_name + '.shp')
-    zipPath = os.path.join(outDir, simple_name + '.zip')
-    with ZipFile(zipPath, 'w') as myzip:
-        myzip.write(inFile)
-    #login to AGOL using pro credentials
-    gis = GIS('pro')
-    # Upload the data using input properties and data location
-    shp = gis.content.add(properties_dict, data= zipPath)
-    #shp.publish()
-
-#---------------------------------------------------------------------------
 #Inputs
-
-inFile = 'H:\\automate_AGOL_download\\AGOL_tests.gdb\\RedLine_datetest' 
-outDir = 'H:\\automate_AGOL_download'
-properties = {
-    'title' : 'Redline_datetest',
-    'tags' : 'Redline, datetest',
-    'type' : 'Shapefile'
-}
+load_dotenv(os.path.join(os.getcwd(), 'environments.env'))
+changesGDB = os.getenv('GEOM_CHANGES_DATA')
+changes_layer = os.getenv('GEOM_LAYER')
+print('GDB source: ' + changesGDB + ' changes fc: ' + changes_layer)
+fl_title= changes_layer + '_' + str(date.today())
 #---------------------------------------------------------------------------
-#Calls
-upload_data(inFile, outDir, properties)
 
-print('DONE!')
+gis = GIS('pro')
+print('Logged in as: ' + str(gis.properties.user.username))
+# delete old versions if they exist
+for item in gis.content.search('title: ' + fl_title):
+    item.delete()
+
+geom_changes = pd.DataFrame.spatial.from_featureclass(os.path.join(changesGDB, changes_layer), sr= '3347')
+print( 'Uploading feature layer with ' + str(len(geom_changes)) + ' records to AGOL')
+
+geom_fl = geom_changes.spatial.to_featurelayer(
+                                    title= fl_title, 
+                                    gis= GIS('pro'), 
+                                    tags= 'NGD_AL, Redline, ' + str(date.today())
+                                    )
+print('Sharing Layer with NGD')
+geom_fl.share( everyone= False, groups= gis.groups.search('title: NGD')[0].groupid)
+print('Upload Complete')
