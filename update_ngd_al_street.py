1d81fd4337656be849cfd8d024413c10f9a49a26
WenkChr <christopher.wenkoff@canada.ca>
2020-07-29 14:30:07 -0400

    Initial working version. Added export to fc test

--- update_ngd_al_street.py
+++ update_ngd_al_street.py
@@ -29,23 +29,32 @@ def table_to_data_frame(in_table, input_fields=None, where_clause=None):
 
 #NGD_AL_fc = r'H:\NGD_AGOL_Download\Final_Export_2020-05-29_2.gdb\WC2021NGD_AL_20200313_'
 NGD_STREET_tbl = r'H:\NGD_AGOL_Download\NGD_Redline.gdb\NGD_STREET'
-NGD_AL_fc = r'H:\NGD_AGOL_Download\NGD_Redline.gdb\al_aliastest'
+NGD_AL_fc = r'H:\NGD_AGOL_Download\Final_Export_2020-05-29_2.gdb\WC2021NGD_AL_20200313_'
 #-------------------------------------------------------------------------------------------------------------------------------------
 #Logic
-print('Loading in NGD_AL and NGD_STREET')
-AL_df = pd.DataFrame.spatial.from_featureclass(NGD_AL_fc)
-STREET_df = table_to_data_frame(NGD_STREET_tbl, ['NGD_STR_UID', 'STR_NME', 'STR_TYP', 'STR_DIR', 'NAME_SRC'])
+print('Loading in NGD_AL')
+AL_df = pd.DataFrame.spatial.from_featureclass(NGD_AL_fc, fields= ['NGD_UID', 'NGD_STR_UID_L', 
+                                                                'ALIAS1_STR_UID_L', 
+                                                                'ALIAS2_STR_UID_L'])
+
 print('Merging NGD_AL and NGD_STREET')
-aliased = pd.DataFrame()
 for alias in ['ALIAS1', 'ALIAS2']:
-    AL_alias = AL_df.loc[AL_df[f'{alias}_STR_UID_L'] != 0]
-    aliasStreet = STREET_df
-    aliasStreet.rename(columns= {'STR_NME' : f'STR_NME_{alias}', 
-                                'STR_TYP' : f'STR_TYP_{alias}',
-                                'STR_DIR' : f'STR_DIR_{alias}',
-                                'STR_NME' : f'STR_NME_{alias}'},
+    print(f'Aliasing {alias} fields')
+    print('Loading in NGD_STREET')
+    aliasStreet = table_to_data_frame(NGD_STREET_tbl, ['NGD_STR_UID', 'STR_NME', 'STR_TYP', 'STR_DIR', 'NAME_SRC'])
+    aliasStreet.rename(columns= {'NGD_STR_UID' : f'{alias}_NGD_STR_UID',
+                                'STR_NME' : f'{alias}_STR_NME', 
+                                'STR_TYP' : f'{alias}_STR_TYP',
+                                'STR_DIR' : f'{alias}_STR_DIR',
+                                'NAME_SRC' : f'{alias}_NME_SRC'},
                                 inplace= True) # Rename NGD_STREET fields to match the alias that they are replacing
-    merged = pd.merge(AL_alias, aliasStreet, left_on= f'{alias}_STR_UID_L', right_on= 'NGD_STR_UID' )
-    print(merged.head())
-    sys.exit()
+    print(f'Merging NGD_STREET to NGD_AL for {alias}')
+    AL_df = AL_df.merge(aliasStreet, how= 'outer', left_on= f'{alias}_STR_UID_L', right_on= f'{alias}_NGD_STR_UID')
+    AL_df.drop(columns= f'{alias}_NGD_STR_UID')
+print('Performing full NGD_STREET join with NGD_AL')
+NGD_STREET_df = table_to_data_frame(NGD_STREET_tbl)
+AL_df = AL_df.merge(NGD_STREET_df, how='outer', left_on='NGD_STR_UID_L', right_on= 'NGD_STR_UID')
+del NGD_STREET_df
+print('Exporting joins to featureclass')
+AL_df.to_featureclass(os.path.join('H:\NGD_AGOL_Download\NGD_Redline.gdb', 'joinPandasTest'), overwrite= True)
 print('DONE!')
